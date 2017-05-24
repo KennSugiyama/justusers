89d3e2dcc56c8250aa9031a2798d8372be739485
Kenn Sugiyama <kenn@KBSugiyama.com>
2017-05-24 01:51:36 -0700

    add associations and validations to models

--- db/migrate/20170524000724_create_users.rb
+++ db/migrate/20170524000724_create_users.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string  :name, { null: false }
+      t.integer :age, { null: false, default: 0 }
+
+      t.timestamps null: false
+    end
   end
 end
